23fd647314b51fd23c4e48b47bc2c6c55c9dbdc6
tangyu <346377510@qq.com>
2018-04-28 14:27:18 +0800

    init

--- main.go
+++ main.go
@@ -14,6 +14,5 @@ func main() {
 	blog.Delims("{{", "}}")
 	blog.LoadHTMLGlob(filepath.Join(os.Getenv("GOPATH"), "src/blog/views/*"))
 	blog.GET("/", controllers.Index)
-	blog.GET("/index.html", controllers.Index)
 	blog.Run(":8080")
 }
