91e100cba00b0b0addb8a83631c7851fb4ce557e
Don <346377510@qq.com>
2018-05-03 22:43:18 +0800

    views

--- framework/db.go
+++ framework/db.go
@@ -1,9 +1,11 @@
 package framework
 
 import (
+	"blog/config"
 	"database/sql"
-	_ "github.com/go-sql-driver/mysql"
 	"sync"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 var once sync.Once
@@ -12,7 +14,9 @@ var Db *sql.DB
 
 func Instance() *sql.DB {
 	once.Do(func() {
-		Db, _ = sql.Open("mysql", "root:123456@tcp(65.49.214.22:3306)/blog?charset=utf8mb4")
+		dataSource := config.Db_username + ":" + config.Db_password +
+			"@" + config.Db_linkType + "(" + config.Db_host + ":" + config.Db_port + ")/" + config.Db_database + "?charset=utf8mb4"
+		Db, _ = sql.Open(config.Db_type, dataSource)
 	})
 	return Db
 }
