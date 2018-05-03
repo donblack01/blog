8d7f4cecb7120db6d30fc844a8a8f42dec48634e
tangyu <346377510@qq.com>
2018-05-03 16:12:04 +0800

    uploads some templates

--- main.go
+++ main.go
@@ -13,6 +13,7 @@ func main() {
 	gin.SetMode(gin.DebugMode)
 	blog.Delims("{{", "}}")
 	blog.LoadHTMLGlob(filepath.Join(os.Getenv("GOPATH"), "src/blog/views/*"))
+	blog.Static("/static", filepath.Join(os.Getenv("GOPATH"), "src/blog/static/"))
 	blog.GET("/", controllers.Index)
 	blog.Run(":8081")
 }
